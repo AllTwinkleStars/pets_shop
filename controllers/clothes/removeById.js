5a9ebcc3aa11e33dcea4f0f9720ced540e1a5a15
AllTwinkleStars <KeepGrowinga@gmail.com>
2023-03-06 19:42:24 +0200

    removeById

--- controllers/clothes/removeById.js
+++ controllers/clothes/removeById.js
@@ -6,17 +6,17 @@ const { destroyToCloudinary } = require("../../service/upload.service");
 const removeById = async (req, res) => {
   const { clothesId } = req.params;
   // const result = await clothesOperations.removeClothes(clothesId);
-  const result = await Cloth.findByIdAndRemove(clothesId);
-  if (!result) {
+  const data = await Cloth.findByIdAndRemove(clothesId);
+  if (!data) {
     throw createError(404, `Product with id=${clothesId} not found`);
   }
 
-  await destroyToCloudinary(result.image.public_id);
+  await destroyToCloudinary(data.image.public_id);
   res.json({
     status: "success",
     code: 200,
     message: "clothes deleted",
-    data: { result },
+    data,
   });
 };
 module.exports = removeById;
