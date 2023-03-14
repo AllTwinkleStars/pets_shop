1eec7d1526bcc76873a9999306f0296ee0f682cf
AllTwinkleStars <KeepGrowinga@gmail.com>
2023-03-14 15:04:14 +0200

    updateById

--- controllers/clothes/updateById.js
+++ controllers/clothes/updateById.js
@@ -8,6 +8,9 @@ const updateById = async (req, res, next) => {
   try {
     const { clothesId } = req.params;
     const { file, body } = req;
+
+    const imageDestroy = await Cloth.findOne({ clothesId });
+    await destroyToCloudinary(imageDestroy.image.public_id);
     const image = await uploadImg(file);
     const result = await Cloth.findByIdAndUpdate(
       clothesId,
@@ -19,8 +22,6 @@ const updateById = async (req, res, next) => {
     if (!result) {
       throw createError(404, `Product with id=${clothesId} not found`);
     }
-
-    await destroyToCloudinary(result.image.public_id);
 
     res.json({
       status: "success",
