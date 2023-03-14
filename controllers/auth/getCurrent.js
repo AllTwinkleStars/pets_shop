66f4cde505f44634ed7a6c3869a79fa18c8429c8
AllTwinkleStars <KeepGrowinga@gmail.com>
2023-03-14 20:29:32 +0200

    1

--- controllers/auth/getCurrent.js
+++ controllers/auth/getCurrent.js
@@ -1,16 +1,20 @@
 // const { User } = require("../../models");
 
 const getCurrent = async (req, res) => {
-  const { name, email, user } = req.user;
-  res.json({
-    status: "success",
-    code: 200,
-    user: {
-      name,
-      email,
-      user,
-    },
-  });
+  try {
+    const { name, email, user } = req.user;
+    res.json({
+      status: "success",
+      code: 200,
+      user: {
+        name,
+        email,
+        user,
+      },
+    });
+  } catch (error) {
+    res.status(401).json({ message: "Current user not found" });
+  }
 };
 
 module.exports = getCurrent;
