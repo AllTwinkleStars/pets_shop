ea8b771d0a3435dc94708e85df43a3a5792fc251
AllTwinkleStars <KeepGrowinga@gmail.com>
2023-03-23 10:06:31 +0200

    1

--- controllers/clothes/getAll.js
+++ controllers/clothes/getAll.js
@@ -8,16 +8,13 @@ const getAll = async (req, res) => {
   //   "owner",
   //   "_id name email"
   // );
-  // const { page = 1, limit = 10 } = req.query;
-  // const skip = (page - 1) * limit;
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
   const clothesAll = await Cloth.find({});
-  const clothes = await Cloth.find(
-    {}
-    //   , "", {
-    //   skip,
-    //   limit: Number(limit),
-    // }
-  ).populate("owner", "_id name email");
+  const clothes = await Cloth.find({}, "", {
+    skip,
+    limit: Number(limit),
+  }).populate("owner", "_id name email");
 
   res.json({
     status: "success",
